9d29541b7e3d8163448bab62fff6c89b16719ffc
Angelo Genovese <angelo@streetcontxt.com>
2018-09-05 14:58:44 -0400

    Got the TreeHugger based generator to the point where it produces working code for the simple test cases

--- src/main/scala/com/contxt/db/JooqTreeHugger.scala
+++ src/main/scala/com/contxt/db/JooqTreeHugger.scala
@@ -9,24 +9,32 @@ import treehuggerDSL._
 import scala.collection.JavaConverters._
 
 object JooqTreeHugger {
-  private val typeMappings = Map[Class[_], Class[_]](
-    classOf[java.lang.Integer] -> classOf[scala.Int],
-    classOf[java.lang.Long] -> classOf[scala.Long],
-    classOf[java.lang.Double] -> classOf[scala.Double],
-    classOf[java.lang.Character] -> classOf[scala.Char],
-    classOf[java.lang.Short] -> classOf[scala.Short],
-    classOf[java.lang.Byte] -> classOf[scala.Byte],
-    classOf[java.lang.Boolean] -> classOf[scala.Boolean],
-    classOf[java.lang.Float] -> classOf[scala.Float],
-    classOf[java.math.BigDecimal] -> classOf[scala.math.BigDecimal],
-    classOf[java.math.BigInteger] -> classOf[scala.math.BigInt]
+  private val typeMappings = Map[Class[_], (String, String)](
+    classOf[java.lang.Integer] -> ("scala.Int", "int2Integer"),
+    classOf[java.lang.Long] -> ("scala.Long", "long2Long"),
+    classOf[java.lang.Double] -> ("scala.Double", "double2Double"),
+    classOf[java.lang.Character] -> ("scala.Char", "char2Char"),
+    classOf[java.lang.Short] -> ("scala.Short", "short2Short"),
+    classOf[java.lang.Byte] -> ("scala.Byte", "byte2Byte"),
+    classOf[java.lang.Boolean] -> ("scala.Boolean", "boolean2Boolean"),
+    classOf[java.lang.Float] -> ("scala.Float", "float2Float"),
+    classOf[java.math.BigDecimal] -> ("scala.math.BigDecimal", "javaBigDecimal2bigDecimal"),
+    classOf[java.math.BigInteger] -> ("scala.math.BigInt", "javaBigInteger2bigInt")
   )
 
+  def fromCatalog(catalog: Catalog, pkg: String): List[forest.PackageDef] = {
+    catalog.getSchemas.asScala.toList.flatMap { s =>
+      val str: String =
+        if (catalog.getName.isEmpty) pkg
+        else pkg + s".${catalog.getName.toLowerCase}"
+      fromSchema(s, str)
+    }
+  }
 
   def fromSchema(schema: Schema, pkg: String): List[forest.PackageDef] = {
-    schema.getTables.asScala.map { t =>
-      fromTable(t, pkg + s".${schema.getName}")
-    }.toList
+    schema.getTables.asScala.toList.map { t =>
+      fromTable(t, pkg + s".${schema.getName.toLowerCase}")
+    }
   }
 
   def fromTable(table: Table[_], pkg: String): forest.PackageDef = {
@@ -41,60 +49,69 @@ object JooqTreeHugger {
     BLOCK(
       CASECLASSDEF(tableName)
         .withParams(
-          snippets.map(_.fieldDecl) ++ snippets.map(_.fieldModFlagDecl)
-        ) := BLOCK(
-
-        DEF("record", recordType) := BLOCK(
-          (VAL("rec", recordType) := NEW(recordType)) +:
-            snippets.flatMap(_.recordSetter(REF("rec"))) :+
-            REF("rec")
-        ),
-        DEF("copy", tableName).withParams(
           snippets.map(_.fieldDecl)
         ) := BLOCK(
-          REF(tableName) APPLY snippets.flatMap(_.fieldCopier)
-        )
+        snippets.map(_.fieldModFlagDecl) ++
+          Array(
+            DEFTHIS
+              .withParams(snippets.map(_.fieldDecl) ++ snippets.map(_.fieldModFlagDecl)) := BLOCK(
+              (THIS APPLY snippets.map(_.fieldAssignment)) +:
+                snippets.map(_.modFlagAssignment)
+
+            ),
+            DEF("record", recordType) := BLOCK(
+              (VAL("rec", recordType) := NEW(recordType)) +:
+                snippets.flatMap(_.recordSetter(REF("rec"))) :+
+                REF("rec")
+            ),
+            DEF("copy", tableName).withParams(
+              snippets.map(_.fieldDecl)
+            ) := BLOCK(
+              NEW(tableName) APPLY snippets.flatMap(_.fieldCopier)
+            )
+          )
       ),
 
       OBJECTDEF(tableName)
         .withParents(table.getClass.getCanonicalName) := BLOCK(
         DEF("fromRecord", tableName)
           .withParams(VAL("rec", recordType).tree) := BLOCK(
-          REF(tableName) APPLY snippets.flatMap(_.recordGetter(REF("rec")))
+          NEW(tableName) APPLY snippets.flatMap(_.recordGetter(REF("rec")))
         )
       )
     ) inPackage pkg
 
   }
 
-  case class FieldSnippets(
-                            fieldDecl: forest.ValDef,
-                            fieldModFlagDecl: forest.ValDef,
-                            recordSetter: forest.Ident => Array[forest.Tree],
-                            recordGetter: forest.Ident => Array[forest.Tree],
-                            fieldCopier: Array[forest.Tree]
-                          )
+  case class FieldSnippets(fieldDecl: forest.ValDef,
+                           fieldModFlagDecl: forest.ValDef,
+                           recordSetter: forest.Ident => Array[forest.Tree],
+                           recordGetter: forest.Ident => Array[forest.Tree],
+                           fieldCopier: Array[forest.Tree],
+                           fieldAssignment: forest.Tree,
+                           modFlagAssignment: forest.Tree)
 
   def fromTableField(tf: TableField[_, _]): FieldSnippets = {
     val dataType: DataType[_] = tf.getDataType
 
-
-    val typ =
+    val typeData = typeMappings.getOrElse(dataType.getType, (dataType.getType.getCanonicalName, "identity"))
+    val typ: String =
       if (dataType.nullable || dataType.defaulted)
-        s"Option[${typeMappings.getOrElse(dataType.getType, dataType.getType).getCanonicalName}]"
-      else typeMappings.getOrElse(dataType.getType, dataType.getType).getCanonicalName
+        s"Option[${typeData._1}]"
+      else typeData._1
 
     val fieldName = underscoreToCamel(tf.getName)
 
     val modifiedField = fieldName + "Modified"
 
     FieldSnippets(
-      VAL(fieldName, typ).tree,
-      VAR(modifiedField, BooleanClass).tree,
-      (rec: forest.Ident) => {
+      fieldDecl = VAL(fieldName, typ).tree,
+      fieldModFlagDecl = VAR(modifiedField, BooleanClass) := LIT(false),
+      recordSetter = (rec: forest.Ident) => {
         val recordSetter =
           if (dataType.nullable || dataType.defaulted)
-            rec DOT ("set" + fieldName.capitalize) APPLY (REF(fieldName) DOT "orNull")
+            rec DOT ("set" + fieldName.capitalize) APPLY (
+              REF(fieldName) DOT "map" APPLY REF(typeData._2) DOT "orNull")
           else rec DOT ("set" + fieldName.capitalize) APPLY REF(fieldName)
 
         Array(
@@ -102,28 +119,28 @@ object JooqTreeHugger {
           rec DOT "changed" APPLY(LIT(tf.getName), REF(modifiedField))
         )
       },
-      (rec: forest.Ident) => {
+      recordGetter = (rec: forest.Ident) => {
         val recordGetter =
           if (dataType.nullable || dataType.defaulted) {
-            REF(fieldName) INFIX "=" APPLY REF("Option") APPLY (rec DOT ("get" + fieldName.capitalize))
+            REF(fieldName) := REF("Option") APPLY (rec DOT ("get" + fieldName.capitalize))
           }
-          else (REF(fieldName) INFIX "=" APPLY rec DOT ("get" + fieldName.capitalize)).tree
+          else REF(fieldName) := rec DOT ("get" + fieldName.capitalize)
 
         Array(
           recordGetter,
-          REF(modifiedField) INFIX "=" APPLY rec DOT "changed" APPLY LIT(tf.getName)
+          REF(modifiedField) := rec DOT "changed" APPLY LIT(tf.getName)
         )
       },
-      Array(
-        THIS DOT fieldName INFIX "=" APPLY REF(fieldName),
-        THIS DOT modifiedField INFIX "=" APPLY (THIS DOT modifiedField INFIX "||"
+      fieldCopier = Array(
+        REF(fieldName) := THIS DOT fieldName,
+        REF(modifiedField) := (THIS DOT modifiedField INFIX "||"
           APPLY (THIS DOT fieldName INFIX "!=" APPLY REF(fieldName)))
-      )
+      ),
+      fieldAssignment = REF(fieldName) := REF(fieldName),
+      modFlagAssignment = THIS DOT modifiedField := REF(modifiedField)
     )
   }
 
   private def underscoreToCamel(name: String) =
     "_([a-z\\d])".r.replaceAllIn(name.toLowerCase, { m => m.group(1).toUpperCase() })
 }
-
-
